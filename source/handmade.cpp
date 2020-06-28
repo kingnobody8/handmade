079f1429c0780fe4cd98c5e5e4ad747332f79fc1
rei\dhabig <danielth101@gmail.com>
2020-06-28 15:01:01 -0500

    day 15 - platform independent debug I/O

--- source/handmade.cpp
+++ source/handmade.cpp
@@ -65,6 +65,15 @@ internal void GameUpdateAndRender(game_memory* Memory, game_input* Input, game_o
 	game_state* GameState = (game_state*)Memory->PermanentStorage;
 	if (!Memory->IsInitialized)
 	{
+		char* Filename = __FILE__;
+
+		debug_read_file_result File = DEBUGPlatformReadEntireFile(Filename);
+		if (File.Contents)
+		{
+			DEBUGPlatformWriteEntireFile("test.out", File.ContentsSize, File.Contents);
+			DEBUGPlatformFreeFileMemory(File.Contents);
+		}
+
 		GameState->ToneHz = 256;
 
 		// TODO(casey): This may be more appropriate to do in the platform layer
